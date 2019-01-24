2df01f5e0f6e1f549ea82947c5b4ebed40b8835b
visualstudio <visualstudio@230-PC33>
2019-01-24 17:22:22 -0500

    4:30 commit code

--- 716490/Program.cs
+++ 716490/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace _716490
+namespace C0716490
 {
     class Program
+
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var dog_and_cat = new Car();
+            var bowling = new Car();
+            Console.WriteLine("how many cars:{0}", Vehicle.howManyCars);
         }
+    }
+
+
+    class Vehicle
+    {
+        public static howManyCars=0; 
+        public string Color = "Blue";
+        internal static int howManyCars;
+    }
+    class Car : Vehicle
+    {
+        public String new int howManyCars = 0;
+        public Car()
+        {
+            Console.WriteLine("my color is " + Color);
+
+        }
+    }
+    class BookCollection
+    {
+
+    }
+    class Book : BookCollection
+    {
+        private String Title;
+        public void SetTitle(String)
     }
 }
